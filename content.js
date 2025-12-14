84fc3c0cb38728cfb7d0bfd8da6417334e2705a6
Eric Holzer <eric.holzer.lsn@gmail.com>
2025-12-14 19:32:11 +0900

    content.js: Update element selectors and improve visibility handling for explore feed manifest.json: Bump version to 1.2.2

--- content.js
+++ content.js
@@ -17,7 +17,7 @@ function showPopupAndStartCountdown(duration) {
         </div>
     </div>
     `;
-
+	
 	// Function to disable page interaction and prevent scrolling
 	function disablePageInteraction() {
 		document.body.style.overflow = 'hidden'; // Prevent scrolling
@@ -78,8 +78,10 @@ function handleContentChanges() {
 
 	// Selector constants
 	const reelsTabSelector = `a[href="/reels/"].x1i10hfl.x1ejq31n.x1hl2dhg`;
-	const homeFeedSelector = '.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x6s0dn4.x1oa3qoh.x1nhvcw1';
-	const exploreFeedSelector = '.x78zum5.xdt5ytf.xwrv7xz.x1n2onr6.xph46j.xfcsdxf.xsybdxg.x1bzgcud';
+	// const homeFeedSelector = '.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x6s0dn4.x1oa3qoh.x1nhvcw1';
+	const homeFeedSelector = '.html-div.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x6s0dn4.x1oa3qoh.x1nhvcw1';
+	const exploreFeedSelector = '.x78zum5.xdt5ytf.x11lt19s.x1n2onr6.xph46j.x7x3xai.xsybdxg.x194l6zq';
+	const exploreFeedLoadingSelector = '.xemfg65.xa4qsjk.x1ka1v4i.xbv57ra';
 	const topBarSelector = '._ab16._ab17';
 	const storiesSelector = '.x1qjc9v5.x78zum5.x1q0g3np.xl56j7k.xh8yej3';
 
@@ -98,6 +100,7 @@ function handleContentChanges() {
 
 	// Conditional display based on the current URL
 	if (currentUrl === 'https://www.instagram.com/') {
+		console.log('home feed', document.querySelector(homeFeedSelector));
 		// check if the setting is enabled
 		chrome.storage.sync.get(['hideMainFeed'], function(result) {
 			if (result.hideMainFeed !== false) {
@@ -150,8 +153,10 @@ function handleContentChanges() {
 		chrome.storage.sync.get(['hideExplorePosts'], function(result) {
 			if (result.hideExplorePosts !== false) {
 				toggleElementVisibility(exploreFeedSelector, 'none');
+				toggleElementVisibility(exploreFeedLoadingSelector, 'none');
 			} else {
 				toggleElementVisibility(exploreFeedSelector, 'block');
+				toggleElementVisibility(exploreFeedLoadingSelector, 'block');
 			}
 		});
 	}
